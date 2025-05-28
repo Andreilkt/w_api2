79359e023df4cee4107644608e049dc17df0d5da
Савчук Андрей <a.savchuk@netrika.ru>
2025-05-28 14:37:10 +0700

    Исправлены импорты

--- app/test_app.py
+++ app/test_app.py
@@ -7,9 +7,9 @@ from app.app import app, City
 @pytest.fixture
 def client():
     # Настройка тестового клиента Flask и базы в памяти
-    # app.config['TESTING'] = True
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-    # app.config['WTF_CSRF_ENABLED'] = False
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['WTF_CSRF_ENABLED'] = False
 
     with app.test_client() as client:
         with app.app_context():
